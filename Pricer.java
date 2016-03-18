606a474675f09bbcb7e8e88fc75218f15ede091c
Adam Alloy <aralloy@gmail.com>
2016-03-18 13:48:29 -0500

    adding updated pricer for merge, still many bugs

--- Pricer.java
+++ Pricer.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 import java.util.TreeSet;
 
 
+
 public class Pricer {
 	private static int targetsize; //fill in from commandline
 	private static TreeSet<Pricer.Entry> OrderBook;
 	public Pricer(){};
-//	private static PrintStream writer;
 	
+	//class which organizes input data
 	public class Entry implements Comparable<Entry>{
 		String oid, sd;
 		double p;
@@ -22,65 +23,78 @@ public class Pricer {
 		this.sz = size;
 		this.time = time;
 		}
+
+		@Override
+		//comparator, has bugs
 		public int compareTo(Entry other) {
-			if(this.oid == other.oid)
-				return 0;
-			if (this.p > other.p)
+			System.out.println("time: " + this.time + " order_id: " + this.oid + " side: " + this.sd + " price: " + this.p + " size: " + this.sz);
+			System.out.println("time: " + other.time + " order_id: " + other.oid + " side: " + other.sd + " price: " + other.p + " size: " + other.sz);
+			
+			if(this.oid.equals(other.oid)){
+	
+					return 0;
+			}
+			if (this.p > other.p){
+
 				return 1;
-			if (this.p < other.p)
+			}
+			if (this.p < other.p){
+
 				return -1;
+			}
+			
+			//likely cause of bugs
 			if(this.p == other.p){
-				//return this.time - other.time;
-				//System.out.println(this.time + " " + this.p + " " + this.oid + " " + this.sd);
-				//if they are both offers to sell, you want to buy the largest amount at the lowest price, so the larger order should be earlier in the book.
-				//if they are both offers to buy, you want to sell the largest amount at the highest price, so the larger order should be later in the book. 
 					if(this.sd.equals(other.sd)){
-						if(this.sd.equals("S"))
-							return ((this.sz >= other.sz) ? 1 : -1);
-						if(this.sd.equals("B"))
-							return ((this.sz >= other.sz) ? -1 : 1);
-					}
+						if(this.sd.equals("S")){
+							return ((this.sz > other.sz) ? 1 : -1);
+						}if(this.sd.equals("B")){
+							return ((this.sz < other.sz) ? -1 : 1);
+						}
+										}
 					else{
 						if(other.sd.equals("S"))
+						{
 							return -1;
-						else
+						}
+						else{
 							return 1;
+						}
 					}
 			
+				
 			}
-
 			return 1;
 		}
 	}
-	
+
 	
 	public static void main(String args[]){
-//		PrintStream writer = new PrintStream(System.out);
 		Scanner tsize = new Scanner(System.in);
 		targetsize = tsize.nextInt();
 		tsize.close();
 		Scanner keyboard = null;
 		try {
+		//hard coded to read from disk, bad style  keyboard = new Scanner(System.in) would be better
 			keyboard = new Scanner(new BufferedReader(new FileReader("/Users/adamalloy/Downloads/pricer.in")));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} // need to remove so not hard coded
+		} 
 		//Scanner keyboard = new Scanner(new BufferedReader(new FileReader("/Users/adamalloy/pricer2.in"))); // need to remove so not hard coded
 		//Scanner keyboard = new Scanner("28800538 A b S 44.26 100\n28800562 A c B 44.10 100\n28800744 R b 100\n28800758 A d B 44.18 157\n28800773 A e S 44.38 100\n28800796 R d 157\n28800812 A f B 44.18 157\n28800974 A g S 44.27 100\n28800975 R e 100\n28812071 R f 100\n28813129 A h B 43.68 50\n28813300 R f 57\n28813830 A i S 44.18 100\n28814087 A j S 44.18 1000\n28814834 R c 100\n28814864 A k B 44.09 100\n28815774 R k 100\n28815804 A l B 44.07 175\n28815937 R j 1000\n28816245 A m S 44.22 100\n28816245 R g 100\n28816273 A n B 44.03 100\n28817570 A o S 44.14 170\n28822172 R o 20\n28823984 A p B 44.04 100\n28823984 R n 100\n28824454 R p 100\n28824484 A q B 44.03 100\n28826314 R q 100\n28826343 A r B 43.89 100\n28826384 R r 100\n28826414 A s B 43.78 100\n28826424 R s 100\n28826454 A t B 43.75 100\n28826455 R t 100\n28826485 A u B 43.72 100\n28835564 A v B 43.85 100\n28835565 R u 100\n28838797 A w S 44.15 500\n28841307 A x S 44.40 100\n28845097 A y S 44.10 500");
 		OrderBook = new TreeSet<Entry>();
 
 		Pricer pricer = new Pricer();
 		Entry current = null;
+		//arrays to hold data about bids and asks so they aren't global
 		int[] BidsAsks = {0,0};
 		boolean[] NA = {false, false};
 		double[] last = {0.0, 0.0};
 		while(keyboard.hasNextLine()){
 			try {
 				processLine(keyboard.nextLine(), BidsAsks, NA, last, pricer, current);
-				//testSet();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -97,20 +111,21 @@ public class Pricer {
 				AddOrder(current, BidsAsks, last, NA);
 			}
 			if(instruction.equals("R")){
+
 				current = ReduceOrder(timestamp, parse.next(), Integer.parseInt(parse.next()), BidsAsks, NA);
 				if(current.sd.equals("B")){
 					last[0] = PrintAdd(current, BidsAsks[0], last[0], OrderBook.descendingIterator());
-//					if(BidsAsks[0] < targetsize)
-//						NA[0] = false;
+
 				}
 				if(current.sd.equals("S")){
 					last[1] = PrintAdd(current, BidsAsks[1], last[1], OrderBook.iterator());
-//					if(BidsAsks[1] < targetsize)
-//						NA[1] = false;
+
 				}
 			}
 }	
+
 		
+//adds order
 private static void AddOrder(Entry current, int[] BidsAsks, double[] last, boolean[] NA) throws IOException {
 		OrderBook.add(current);
 		if(current.sd.equals("B")){
@@ -127,11 +142,8 @@ private static void AddOrder(Entry current, int[] BidsAsks, double[] last, boole
 	}
 }
 
-//		writer.flush();
-//		writer.close();
-	
 
-
+//prints adds
 	private static double PrintAdd(Entry current, int num, double last, Iterator<Entry> iter) {
 		double total = 0.0;
 		if (num >= targetsize){
@@ -139,7 +151,6 @@ private static void AddOrder(Entry current, int[] BidsAsks, double[] last, boole
 			Entry tmp; 
 			while(count > 0 && iter.hasNext()){
 				tmp = iter.next();
-				//System.out.println("time: " + tmp.time + " order_id: " + tmp.oid + " side: " + tmp.sd + " price: " + tmp.p + " size: " + tmp.sz);
 				if(tmp.sd.equals(current.sd)){
 					if((count - tmp.sz) > 0){
 						count -= tmp.sz;
@@ -162,7 +173,6 @@ private static void AddOrder(Entry current, int[] BidsAsks, double[] last, boole
 }
 
 	private static Pricer.Entry ReduceOrder(int timestamp, String O_id, int size, int[] BidsAsks, boolean[] NA) {
-		// TODO Auto-generated method stub
 		Iterator<Entry> iter =  OrderBook.iterator();
 		Entry tmp = null;
 		String tmp_id = "";
@@ -175,6 +185,7 @@ private static void AddOrder(Entry current, int[] BidsAsks, double[] last, boole
 		else
 			BidsAsks[1] -= ((tmp.sz-size) <= 0) ? tmp.sz : size;
 		if((tmp.sz-size) <= 0){
+			System.out.println(tmp.oid + " " + O_id);
 			OrderBook.remove(tmp);	
 		}
 		else{
@@ -188,54 +199,18 @@ private static void AddOrder(Entry current, int[] BidsAsks, double[] last, boole
 			System.out.println(timestamp + " " + (tmp.sd.equals("S") ? "B" : "S") + " NA");
 			NA[1] = false;
 		}
-			
+		if(timestamp == 29070543){
+			System.out.println("29070543");
+			//testSet();
+		}
+
 		tmp.time = timestamp;
 		return tmp;
 	}
 
 	
 
-/*	private static double PrintOrder(Entry current, int num, double last) throws IOException  {
-		// TODO Auto-generated method stub
-		//testSet();
-		Iterator<Entry> iter;
-		if (num >= targetsize){
-			
-			int count = targetsize;
-			double total = 0.00;
-			if(current.sd.equals("S")){
-				iter =  OrderBook.iterator();
-			}
-			else{
-				iter = OrderBook.descendingIterator();
-			}
-			Entry tmp; 
-			while(count > 0 && iter.hasNext()){
-				tmp = iter.next();
-
-				if(tmp.sd.equals(current.sd)){
-					if((count - tmp.sz) > 0){
-						count -= tmp.sz;
-						total += (tmp.p*tmp.sz);
-					}
-					else{
-						total += (tmp.p*count);
-						count = 0;
-					}
-					
-				}
-			}
-			if(total != last){
-			DecimalFormat decim = new DecimalFormat("0.00");
-			String t = decim.format(total);
-			System.out.println(current.time + " " + (current.sd.equals("S") ? "B" : "S")  + " " + t);
-			}
-			return total;
-		}
-		else
-			return 0.0;
-	}*/
-	
+	//a method that prints the orderbook
 	private static void testSet(){
 		Entry tmp = null;
 		Iterator<Entry> iter = OrderBook.iterator();
